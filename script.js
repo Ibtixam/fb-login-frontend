ec89b8bf3239ac2bcd8af979d64510bb13dee824
M. Ibtisam <ibtisamkhan524@gmail.com>
2023-12-02 03:58:20 +0500

    POST Fields in Database Fetch Api

---
+++ script.js
@@ -0,0 +1,17 @@
+const Login = () => {
+  const formData = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+
+  const url = "http://localhost:5000/submit";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then(async (response) => await response.json())
+    .catch((error) => console.log(error));
+};
